fc4fcb24a29b2dcea09f80158a52a40dbc22348d
andy fang <andy@mingtak.com.tw>
2016-04-08 04:17:08 +0100

    initial for i8d

---
+++ mingtak/oauthlogin/browser/oauthLogin.py
@@ -0,0 +1,167 @@
+from Products.Five.browser import BrowserView
+import logging
+from plone import api
+from requests_oauthlib import OAuth2Session
+from requests_oauthlib.compliance_fixes import facebook_compliance_fix
+import urllib2
+from zope.component import getUtility, queryUtility
+from plone.registry.interfaces import IRegistry
+from Products.CMFPlone.utils import safe_unicode
+from oauthlib.oauth2 import TokenExpiredError
+#from plone import namedfile
+import os; os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+logger = logging.getLogger("mingtak.oauthlogin.browser.oauthLogin")
+
+class OauthWorkFlow(object):
+    prefixString = "mingtak.oauthlogin.oauth2login.IOauth2Setting."
+    oauthServerName = ""
+
+    def __init__(self, oauthServerName):
+        self.oauthServerName = oauthServerName
+
+    def getRegistryValue(self):
+        prefixString, oauthServerName = self.prefixString, self.oauthServerName
+        registry = getUtility(IRegistry)
+        client_id = registry.get("%s%s%s" % (prefixString, oauthServerName, "AppId"))
+        client_secret = registry.get("%s%s%s" % (prefixString, oauthServerName, "AppSecret"))
+        scope = registry.get("%s%s%s" % (prefixString, oauthServerName, "Scope"))
+        redirect_uri = registry.get("%s%s%s" % (prefixString, oauthServerName, "RedirectUri"))
+        return client_id, client_secret, scope, redirect_uri
+
+    def getUserInfo(self, oauth2Session, token_url, client_secret, code, getUrl):
+        oauth2Session.fetch_token(token_url=token_url,
+                                  client_secret=client_secret,
+                                  code=code)
+        getUser = oauth2Session.get(getUrl)
+        return getUser
+
+    def createUser(self, userid, email, properties):
+        if not email:
+            email = u'user@opptoday.com'
+        user = api.user.create(username=userid, email=email, properties=properties,)
+#        import pdb; pdb.set_trace()
+#        if user.getProperty('picture'):
+#            pictureRaw = urllib2.urlopen(user.getProperty('picture')).read()
+#            namedfile.NamedBlobImage(data=pictureRaw, filename=u'portrait.png')
+        return
+
+
+class FacebookLogin(BrowserView):
+    token_url = "https://graph.facebook.com/oauth/access_token"
+    authorization_base_url = "https://www.facebook.com/dialog/oauth"
+    getUrl = "https://graph.facebook.com/me?"
+
+    def __call__(self):
+        oauthWorkFlow = OauthWorkFlow(oauthServerName="facebook")
+        client_id, client_secret, scope, redirect_uri = oauthWorkFlow.getRegistryValue()
+        code = getattr(self.request, 'code', None)
+        facebook = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+        facebook = facebook_compliance_fix(facebook)
+        if code == None:
+            if hasattr(self.request, 'error'):
+                self.request.response.redirect("/")
+                return
+            authorization_url, state = facebook.authorization_url(self.authorization_base_url)
+            self.request.response.redirect(authorization_url)
+            return
+        user = oauthWorkFlow.getUserInfo(facebook, self.token_url, client_secret, code, self.getUrl).json()
+
+        # check has id, if True, is a relogin user, if False, is a new user
+        userid = safe_unicode("fb%s") % user["id"]
+        if api.user.get(userid=userid) is not None:
+            self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
+            self.request.RESPONSE.redirect("/")
+            return
+        userInfo = dict(
+            fullname=safe_unicode(user.get("name", "")),
+            description=safe_unicode(user.get("about", "")),
+            location=safe_unicode(user.get("locale", "")),
+            fbGender=safe_unicode(user.get("gender", "")),
+            home_page=safe_unicode(user.get("link", "")),
+        )
+        oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
+        self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
+        self.request.RESPONSE.redirect("/")
+        return
+
+
+class GoogleLogin(BrowserView):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    authorization_base_url = "https://accounts.google.com/o/oauth2/auth"
+    getUrl = "https://www.googleapis.com/oauth2/v1/userinfo"
+
+    def __call__(self):
+        oauthWorkFlow = OauthWorkFlow(oauthServerName="google")
+        client_id, client_secret, scope, redirect_uri = oauthWorkFlow.getRegistryValue()
+        scope = scope.split(',')
+        code = getattr(self.request, 'code', None)
+        google = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+        if code == None:
+            if hasattr(self.request, 'error'):
+                self.request.response.redirect("/")
+                return
+            authorization_url, state = google.authorization_url(self.authorization_base_url)
+            self.request.response.redirect(authorization_url)
+            return
+        user = oauthWorkFlow.getUserInfo(google, self.token_url, client_secret, code, self.getUrl).json()
+        # check has id, if True, is a relogin user, if False, is a new user
+        userid = safe_unicode("gg%s") % user["id"]
+        if api.user.get(userid=userid) is not None:
+            self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
+            self.request.RESPONSE.redirect("/")
+            return
+        userInfo = dict(
+            fullname=safe_unicode(user.get("name", "")),
+            location=safe_unicode(user.get("locale", "")),
+            fbGender=safe_unicode(user.get("gender", "")),
+            home_page=safe_unicode(user.get("link", "")),
+            family_name=safe_unicode(user.get("family_name", "")),
+            picture=safe_unicode(user.get("picture", "")),
+            verified_email=safe_unicode(user.get("verified_email", False)),
+        )
+        oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
+        self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
+        self.request.RESPONSE.redirect("/")
+        return
+
+
+class TwitterLogin(BrowserView):
+    token_url = ""
+    authorization_base_url = ""
+    getUrl = ""
+
+    def __call__(self):
+        oauthWorkFlow = OauthWorkFlow(oauthServerName="twitter")
+        client_id, client_secret, scope, redirect_uri = oauthWorkFlow.getRegistryValue()
+        scope = scope.split(',')
+        code = getattr(self.request, 'code', None)
+        twitter = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+        if code == None:
+            if hasattr(self.request, 'error'):
+                self.request.response.redirect("/")
+                return
+            authorization_url, state = twitter.authorization_url(self.authorization_base_url)
+            self.request.response.redirect(authorization_url)
+            return
+        user = oauthWorkFlow.getUserInfo(twitter, self.token_url, client_secret, code, self.getUrl).json()
+        # check has id, if True, is a relogin user, if False, is a new user
+        userid = safe_unicode("gg%s") % user["id"]
+        if api.user.get(userid=userid) is not None:
+            self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
+            self.request.RESPONSE.redirect("/")
+            return
+        userInfo = dict(
+            fullname=safe_unicode(user.get("name", "")),
+            location=safe_unicode(user.get("locale", "")),
+            fbGender=safe_unicode(user.get("gender", "")),
+            home_page=safe_unicode(user.get("link", "")),
+            family_name=safe_unicode(user.get("family_name", "")),
+            picture=safe_unicode(user.get("picture", "")),
+            verified_email=safe_unicode(user.get("verified_email", False)),
+        )
+        oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
+        self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
+        self.request.RESPONSE.redirect("/")
+        return
---
+++ mingtak/oauthlogin/oauth2login.py
@@ -0,0 +1,122 @@
+from five import grok
+
+from z3c.form import group, field
+from zope import schema
+from zope.interface import invariant, Invalid
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from plone.dexterity.content import Container
+from plone.directives import dexterity, form
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.interfaces import IImageScaleTraversable
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+
+from mingtak.oauthlogin import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+allowWebsite = SimpleVocabulary(
+    [SimpleTerm(value=u'facebook', title=_(u'facebook')),
+     SimpleTerm(value=u'google', title=_(u'google')),
+     SimpleTerm(value=u'twitter', title=_(u'twitter')),]
+)
+"""
+     SimpleTerm(value=u'linkedin', title=_(u'linkedin')),
+     SimpleTerm(value=u'twitter', title=_(u'twitter'))]
+)
+"""
+
+
+class IOauth2Setting(form.Schema, IImageScaleTraversable):
+    """
+    Oauth2 login method
+    """
+    allowList = schema.List(
+        title=_(u"Allow List"),
+        description=_(u"Allow list that select website to login."),
+        value_type=schema.Choice(
+                       title=_(u"Allow website"),
+                       vocabulary=allowWebsite,
+                       required=False,
+                   ),
+        required=False,
+    )
+
+    facebookAppId = schema.TextLine(
+        title=_(u"Facebook app id"),
+        required=False,
+    )
+
+    facebookAppSecret = schema.TextLine(
+        title=_(u"Facebook app secret"),
+        required=False,
+    )
+
+    facebookScope = schema.TextLine(
+        title=_(u"Facebook auth scope"),
+        required=False,
+    )
+
+    facebookRedirectUri = schema.URI(
+        title=_(u"Facebook redirect URL"),
+        required=False,
+    )
+
+    googleAppId = schema.TextLine(
+        title=_(u"Google app id"),
+        required=False,
+    )
+
+    googleAppSecret = schema.TextLine(
+        title=_(u"Google app secret"),
+        required=False,
+    )
+
+    googleScope = schema.TextLine(
+        title=_(u"Google auth scope"),
+        required=False,
+    )
+
+    googleRedirectUri = schema.URI(
+        title=_(u"Google redirect URL"),
+        required=False,
+    )
+    """
+    twitterAppId = schema.TextLine(
+        title=_(u"Twitter app id"),
+        required=False,
+    )
+
+    twitterAppSecret = schema.TextLine(
+        title=_(u"Twitter app secret"),
+        required=False,
+    )
+
+    twitterScope = schema.TextLine(
+        title=_(u"Twitter auth scope"),
+        required=False,
+    )
+
+    twitterRedirectUri = schema.URI(
+        title=_(u"Twitter redirect URL"),
+        required=False,
+    )"""
+
+
+class Oauth2Login(Container):
+    grok.implements(IOauth2Setting)
+
+
+class SampleView(grok.View):
+    """ sample view class """
+
+    grok.context(IOauth2Setting)
+    grok.require('zope2.View')
+    grok.name('view')
